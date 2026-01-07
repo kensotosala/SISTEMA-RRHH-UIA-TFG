348ac4b3ebb2d7138415ea8df32ef2c9324db2f0
Kendall Salazar <kendallsalazarsoto@gmail.com>
2026-01-06 19:11:32 -0600

    UI

--- TFG-SISTEMA-RRHH/BusinessLogicLayer/obj/Debug/net10.0/BusinessLogicLayer.AssemblyInfo.cs
+++ TFG-SISTEMA-RRHH/BusinessLogicLayer/obj/Debug/net10.0/BusinessLogicLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a1163b304728669e51219c29a3b064ee8c5e107")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72202f24bd5dbe48b1e440f929e96f44f51dcceb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TFG-SISTEMA-RRHH/PresentationLayer/obj/Debug/net10.0/PresentationLayer.AssemblyInfo.cs
+++ TFG-SISTEMA-RRHH/PresentationLayer/obj/Debug/net10.0/PresentationLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a1163b304728669e51219c29a3b064ee8c5e107")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72202f24bd5dbe48b1e440f929e96f44f51dcceb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
